2edf0b7636f061d22612c8226b79c0c3ca355142
davikr7 <david.krasnov7@gmail.com>
2023-02-09 14:16:22 +0100

    added new link 2

--- src/test/java/GitConflict.java
+++ src/test/java/GitConflict.java
@@ -6,6 +6,6 @@ import static com.codeborne.selenide.Selenide.open;
 public class GitConflict {
     @Test
     void makeConflict() {
-        open("https://www.youtube.com/");
+        open("https://www.sports.com/");
     }
 }
